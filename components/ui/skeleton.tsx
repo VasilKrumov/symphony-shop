9303aac6389a4e8a44a5f2b9c28cc22906a229f0
vasil.krumov <vassil.krumov@gmail.com>
2025-11-02 10:31:20 +0200

    Fixed loading state issues

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -2,7 +2,7 @@ import type React from "react"
 import { cn } from "@/lib/utils"
 
 function Skeleton({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) {
-  return <div className={cn("bg-muted animate-shimmer rounded", className)} {...props} />
+  return <div className={cn("bg-muted animate-pulse rounded", className)} {...props} />
 }
 
 export { Skeleton }
